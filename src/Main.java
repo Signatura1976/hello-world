5922c34d399b640568fdab6fadf5c9ec9f89fc41
signatura1976 <ignatyev76@gmail.com>
2024-07-03 20:59:53 +0400

    Написал еще чего-то

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("Написал что-то ещё");
+        System.out.println("Опять написал чего-то");
     }
 }
