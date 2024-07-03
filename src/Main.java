a08ad58109aae6330468afdf67febbe1f929fc6d
signatura1976 <ignatyev76@gmail.com>
2024-07-03 21:13:53 +0400

    всё стёр

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
-        System.out.println("Написал что-то ещё");
-        System.out.println("Опять написал чего-то");
+        System.out.println("Vsyo ster");
+
     }
 }
